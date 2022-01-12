8bbd1688fb7c2aefd4725046e9e77739d23c4b10
octaprog7 <goctaprog@gmail.com>
2022-01-12 16:06:37 +0500

    ---

--- fib.py
+++ fib.py
@@ -67,6 +67,7 @@ class Fibonacci:
 
 
 if __name__ == "__main__":
-    F = Fibonacci()
-    L = [i for i in F if i % 2 == 0 and i < 400]
-    print(f"sum: {sum(L)}")
+    F = Fibonacci(max_value=4_000_000)
+    # выражение-генератор
+    lg = (i for i in F if i % 2 == 0 and i < 4_000_000)
+    print(f"sum: {sum(lg)}")
