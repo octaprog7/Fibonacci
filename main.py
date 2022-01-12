d351d7da2df607216eb8d162e802f975801df683
Roman <goctaprog@gmail.com>
2022-01-12 16:06:21 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from fib import Fibonacci
-
-
-if __name__ == '__main__':
-    F = Fibonacci(max_value=4_000_000)
-    # выражение-генератор
-    lg = (i for i in F if i % 2 == 0 and i < 4_000_000)
-    print(f"sum: {sum(lg)}")
